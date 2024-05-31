58c5049b26ae7c80a8668a20cb8bb6a187909a30
Alexandru Afilom <alexandru.afilom02@e-uvt.ro>
2024-05-31 22:04:06 +0300

    Changed max simulation speed from 5 to 10, added speed slider text and a reset button

--- Assets/GlobalUIHandler.cs
+++ Assets/GlobalUIHandler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,7 @@ public class GlobalUIHandler : MonoBehaviour
 {
     [SerializeField] Slider slider;
     [SerializeField] GameObject earth;
+    [SerializeField] TMP_Text textObj;
 
     // Start is called before the first frame update
     void Start()
@@ -24,8 +26,8 @@ public class GlobalUIHandler : MonoBehaviour
 
     public void ValueChangeCheck()
     {
-        if(slider == null || earth == null)
+        if(slider == null || earth == null || textObj == null)
             return;
-        Debug.Log(slider.value);
+        textObj.text = "Viteza de simulare: x" + slider.value;
     }
 }
